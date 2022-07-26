2166cb1c9eddc76894ef762bdc846a52de3e1ee2
Carla Romero Albornoz <95144975+carla-r-albornoz@users.noreply.github.com>
2022-07-25 23:58:01 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -54,34 +54,4 @@ function inclusivo() {
         document.getElementById("card2").style.backgroundColor="#F2F4F4";
         document.getElementById("card3").style.backgroundColor="#F2F4F4";
         document.getElementById("card4").style.backgroundColor="#F2F4F4";
-        /*
-        document.body.style.backgroundColor="black";
-        document.getElementById("origen").style.color="gray";
-        document.getElementById("epi").style.color="white";
-        document.getElementById("cocinar").style.color="grey";
-        document.getElementById("ingredientes").style.color="white";
-        document.getElementById("masa").style.color="white";
-        document.getElementById("relleno").style.color="white";
-        document.getElementById("salsa").style.color="white";
-        document.getElementById("procedimiento").style.color="white";
-        document.getElementById("lamasa").style.color="white";
-        document.getElementById("elrelleno").style.color="white";
-        document.getElementById("formar").style.color="white";
-        document.getElementById("infoa").style.color="grey";
-        document.getElementById("parrafo").style.color="white";
-        */
-        /*
-        document.getElementById("aleman").style.backgroundColor="black";
-        document.getElementById("tiramisu").style.backgroundColor="black";
-        document.getElementById("lemon").style.backgroundColor="black";
-        document.getElementById("cheesecake").style.backgroundColor="black";
-        document.getElementById("turrontitulo").style.color="grey";
-        document.getElementById("turronparrafo").style.color="white";
-        document.getElementById("tiramisutitulo").style.color="grey";
-        document.getElementById("tiramisuparrafo").style.color="white";
-        document.getElementById("lemontitulo").style.color="grey";
-        document.getElementById("lemonparrafo").style.color="white";
-        document.getElementById("caketitulo").style.color="grey";
-        document.getElementById("cakeparrafo").style.color="white";
-        */
         }
